14fbabc747d4679d680a3b538cfe071a52805f9d
petchiamdocs <petchiappanitt@gmail.com>
2024-07-18 12:26:27 +0530

    first commit

---
+++ src/main/java/com/example/spring_boot_hello/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String greet() {
+    	return "Hello There";
+    }
+}
